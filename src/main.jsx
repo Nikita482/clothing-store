ef877ec46579dbe2e00cdf57c115b2e58e583058
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-18 16:22:33 +0300

    3

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router basename='/clothing-store'>
+      <Link to="/">123</Link>
+
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
