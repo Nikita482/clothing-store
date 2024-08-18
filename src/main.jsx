2bccc3bb052e31180af31c2a56b6aa6557546f6a
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-18 16:32:02 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Page1 from './pages/page1.jsx'
 
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router basename='/clothing-store'>
-      <Link to="/">123</Link>
 
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='/Page1' element={<Page1 />} />
       </Routes>
     </Router>
   </StrictMode>,
