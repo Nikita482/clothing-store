78ad10bee1960d20ebcc60905ad4d03c938515a2
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-19 18:10:55 +0300

    header

---
+++ src/Full/Layout/Layout.jsx
@@ -0,0 +1,64 @@
+import styles from "./Layout.module.css"
+
+import { useState } from "react"
+
+
+export default function Layout(){
+    
+    const[window, setWindow] = useState(true)
+    const menu = () =>{
+        setWindow(!window)
+
+    }
+
+    return(
+        <>
+            <div className={window ? styles.windowVisible : styles.windowNotVisible}>
+                <div className={styles.socials}>
+                    <div className={styles.vk}></div>
+                    <div className={styles.inst}></div>
+                    <div className={styles.tg}></div>
+                    <div className={styles.youTube}></div>
+                </div>
+
+                <div className={styles.btnBox}>
+                    <button className={styles.chapter}> Мужская одежда</button>
+                    <button className={styles.chapter}>Женская одежда</button>
+                    <button className={styles.chapter}>Помощь</button>
+                    <button className={styles.chapter}>Связь с нами</button>
+                    <button onClick={menu} className={styles.chapterBack}>Закрыть</button>
+                </div>
+            </div>
+
+
+            <div className={styles.wrapper}>
+                <div className={styles.box}>
+                    <h1 className={styles.name}>GlamGarb</h1>
+
+                    <div onClick={menu} className={styles.btnMenu}>☰</div>
+                    
+                </div>
+
+                <div className={styles.menu}>
+                    <div className={styles.leftBox}>
+                        <button className={styles.leftBtn}> Мужская одежда</button>
+                        <button className={styles.CenterBtn}>Женская одежда</button>
+                        <button className={styles.CenterBtn}>Помощь</button>
+                        <button className={styles.CenterBtn}>Связь с нами</button>
+                    </div>
+
+                    <div className={styles.rightBox}>
+                        <div className={styles.vk}></div>
+                        <div className={styles.inst}></div>
+                        <div className={styles.tg}></div>
+                        <div className={styles.youTube}></div>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+// ☰
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
-import Page1 from './pages/page1.jsx'
-
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
+import Full from './Full/Full.jsx'
+// import Page1 from './pages/page1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router basename='/clothing-store'>
-
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/Page1' element={<Page1 />} />
+        <Route path='/' element={<Full />} />
+        {/* <Route path='/Page1' element={<Page1 />} /> */}
       </Routes>
     </Router>
   </StrictMode>,
