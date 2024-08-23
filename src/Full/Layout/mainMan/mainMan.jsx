a6a9a19aaa760e7a4a2cf844240ec7fde18f312e
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-23 13:09:34 +0300

    main man

--- src/Full/Layout/mainMan/mainMan.jsx
+++ src/Full/Layout/mainMan/mainMan.jsx
@@ -6,25 +6,29 @@ import { Link } from "react-router-dom"
 export default function MainMan(){
     return(
         <>
-            <div className={styles.main}>
-                <div className={styles.mainLeftBox}>
-                    <div className={styles.mainLeftBoxTop}>
-                        <Link to="/Product" className={styles.mainLeftBoxTopLeft}>Футболки</Link>
+            <div className={styles.wrapper}>
+                <div className={styles.fotoTop}>
+                    <div className={styles.chapter1}>
 
-                        <div className={styles.mainLeftBoxTopRight}>
-                            <Link to="/Product" className={styles.mainLeftBoxTopRightTop}>Худи</Link>
-                            <Link to="/Product" className={styles.mainLeftBoxTopRightBottom}>Куртки</Link>
-                        </div>
                     </div>
-                    
-                    <div className={styles.mainLeftBoxBottom}>
+
+                    <div className={styles.chapter2}>
 
                     </div>
                 </div>
 
-                <div className={styles.mainRightBox}>
-                    <Link to="/Product" className={styles.mainRightBoxTop}>Джинсы</Link>
-                    <Link to="/Product" className={styles.mainRightBoxBottom}>123</Link>
+                <div className={styles.fotoBottom}>
+                    <div className={styles.chapter3}>
+
+                    </div>
+
+                    <div className={styles.chapter4}>
+
+                    </div>
+
+                    <div className={styles.chapter5}>
+
+                    </div>
                 </div>
             </div>
         </>
