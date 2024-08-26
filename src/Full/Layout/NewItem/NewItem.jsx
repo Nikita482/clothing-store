846e79db40c337216ea81b4d459f6b710cec8e07
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-26 18:48:05 +0300

    new item

--- src/Full/Layout/NewItem/NewItem.jsx
+++ src/Full/Layout/NewItem/NewItem.jsx
@@ -10,11 +10,11 @@ export default function NewItem(){
                 <h1 className={styles.name}>Новинки</h1>
 
                 <div className={styles.items}>
-                    <Link to="/Product" className={styles.item1}>123</Link>
-                    <Link to="/Product" className={styles.item2}>123</Link>
-                    <Link to="/Product" className={styles.item3}>123</Link>
-                    <Link to="/Product" className={styles.item4}>123</Link>
-                    <Link to="/Product" className={styles.item5}>123</Link>
+                    <Link to="/Product" className={styles.item1}><span className={styles.text}>Футболки</span></Link>
+                    <Link to="/Product" className={styles.item2}><span className={styles.text}>Худи</span></Link>
+                    <Link to="/Product" className={styles.item3}><span className={styles.text}>Джинсы</span></Link>
+                    <Link to="/Product" className={styles.item4}><span className={styles.text}>Куртки</span></Link>
+                    <Link to="/Product" className={styles.item5}><span className={styles.text}>Аксессуары</span></Link>
                 </div>
             </div>
         </>
